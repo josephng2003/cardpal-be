fd47f604009468c5015b3c33e109637f1adce831
Phong Nguyen <andyhoaiphong2003@gmail.com>
2026-07-01 01:24:24 +0700

    Initial commit

---
+++ src/main/java/decoteam/cardpal/CardpalApplication.java
@@ -0,0 +1,13 @@
+package decoteam.cardpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardpalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardpalApplication.class, args);
+    }
+
+}
